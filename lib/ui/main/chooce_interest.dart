9f62c795fa9f3849f0a652374f1c4634212181b6
muhammednashat <mnashat.dev@gmail.com>
2025-08-13 12:12:27 +0300

    set up main featrue

---
+++ lib/ui/main/chooce_interest.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ChooceInterestScreen extends StatelessWidget {
+  const ChooceInterestScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('data'),),
+    );
+  }
+}
